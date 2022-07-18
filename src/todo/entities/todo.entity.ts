285ca5cdeaafcf0b53614238832e5737315522b2
BebkoIvan <ivan.bebko@medondo.com>
2022-07-18 14:09:55 +0200

    added date to todo

--- src/todo/entities/todo.entity.ts
+++ src/todo/entities/todo.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Todo {
@@ -10,4 +10,7 @@ export class Todo {
 
     @Column({ default: false })
     completed: boolean;
+
+    @CreateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP(6)" })
+    created_at: Date
 }
